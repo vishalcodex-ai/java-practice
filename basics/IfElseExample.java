8897c4bac267c940d4bfe80ddb660f3a07d9cae6
Vishal Choudhary <vishalch5844@gmail.com>
2026-01-25 16:54:37 +0530

    Add IfElseExample class to check positive or negative

---
+++ basics/IfElseExample.java
@@ -0,0 +1,11 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Negative number");
+        }
+    }
+}
