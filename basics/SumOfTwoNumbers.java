1ae08a09b0454cb114eb7dff1f83de21bf07e6e9
Vishal Choudhary <vishalch5844@gmail.com>
2026-01-25 16:49:04 +0530

    Add SumOfTwoNumbers class to calculate sum

---
+++ basics/SumOfTwoNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
